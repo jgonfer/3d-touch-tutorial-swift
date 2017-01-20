1f025685ebd8eaeb80aad768992611cb13d90dfc
Josep Gonzalez <josep.gonfer@gmail.com>
2017-01-20 10:12:05 +0100

    Empty project with tabs

---
+++ touch 3d/touch 3d/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  touch 3d
+//
+//  Created by jgonzalez on 20/1/17.
+//  Copyright © 2017 jgonfer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
